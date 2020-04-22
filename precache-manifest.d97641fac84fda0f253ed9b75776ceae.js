f096c96e3fdd38dba0f002c782d928de651ab75f
ho.viet.tuan <ho.viet.tuan@sun-asterisk.com>
2020-04-22 22:01:13 +0700

    Updates

---
+++ precache-manifest.d97641fac84fda0f253ed9b75776ceae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c9ecccc0df2ca3d8c9ff96bfc65726",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "d1818228a11a6fde63aa",
+    "url": "/react-blog-github/static/css/main.f578dd71.chunk.css"
+  },
+  {
+    "revision": "6ef9f23dd493ebaf9d92",
+    "url": "/react-blog-github/static/js/2.8cba2910.chunk.js"
+  },
+  {
+    "revision": "95dac241b46b34feae5bea722f7e59af",
+    "url": "/react-blog-github/static/js/2.8cba2910.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1818228a11a6fde63aa",
+    "url": "/react-blog-github/static/js/main.2aaeab3a.chunk.js"
+  },
+  {
+    "revision": "177bd86bebe590b64543",
+    "url": "/react-blog-github/static/js/runtime-main.442d08e4.js"
+  },
+  {
+    "revision": "65f8709df0f0bfa7d0a05d7f9517ba07",
+    "url": "/react-blog-github/static/media/loader.65f8709d.svg"
+  }
+]);
